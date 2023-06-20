c61f84f984ab5dfed9d0df7afbcbd09db5839a67
yulat214 <shigineko64@yahoo.co.jp>
2023-06-20 15:22:54 +0900

    ren 4th

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,12 @@ import pygame as pg
 from random import randrange
 
 WIDTH, HEIGHT = 1600, 900
-
+key_dct = {
+    pg.K_UP : (0, -5),
+    pg.K_DOWN : (0, 5),
+    pg.K_LEFT : (-5, 0),
+    pg.K_RIGHT : (5, 0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,7 +17,6 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     
-    # 練習問題3, こうかとん設定
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
 
@@ -34,17 +38,25 @@ def main():
             if event.type == pg.QUIT: 
                 return
         
-        # 練習問題3, 移動量
         key_lst = pg.key.get_pressed()
         move_lst = [0, 0]
-        if key_lst[pg.K_UP]: move_lst[1] -= 5
-        if key_lst[pg.K_DOWN]: move_lst[1] += 5
-        if key_lst[pg.K_LEFT]: move_lst[0] -= 5
-        if key_lst[pg.K_RIGHT]: move_lst[0] += 5
+        
+        for k, v in key_dct.items():
+            if key_lst[k]:
+                move_lst[0] += v[0]
+                move_lst[1] += v[1]
             
         kk_rct.move_ip(move_lst)
+        if in_or_out(kk_rct) != (True, True):
+            kk_rct.move_ip(-1*move_lst[0], -1*move_lst[1])
+            
         bomb_rct.move_ip(vx, vy)
-        
+        v, h = in_or_out(bomb_rct)
+        if not v:
+            vy *= -1
+        if not h:
+            vx *= -1
+            
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         
@@ -54,7 +66,23 @@ def main():
         tmr += 1
         # original -> clock.tick(10)
         clock.tick(50)
+        
+# 練習問題4
+def in_or_out(rct: pg.Rect):
+    """_こうかとん, 爆弾のRectが画面外(False)か画面内(True)か判断
 
+    Args:
+        rct: rectオブジェクト
+
+    Returns:
+        _type_: 真理値のタプル(たて, よこ)
+    """
+    v, h = True, True
+    if rct.right > WIDTH or rct.left < 0:
+        h = False
+    if rct.top < 0 or rct.bottom > HEIGHT:
+        v = False
+    return v, h
 
 if __name__ == "__main__":
     pg.init()

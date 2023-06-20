8eb7798646542309d2c62c5a2783be97e16514f5
yulat214 <shigineko64@yahoo.co.jp>
2023-06-20 14:05:18 +0900

    ren 1st

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 
+from random import randrange
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -8,8 +9,17 @@ WIDTH, HEIGHT = 1600, 900
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("./fig/pg_bg.jpg")
-    kk_img = pg.image.load("./fig/3.png")
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex02/fig/3.png")
+    
+    # 以下練習問題1, 設定
+    bomb = pg.Surface((20, 20))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
+    bomb.set_colorkey((0, 0, 0))
+    bomb_rct = bomb.get_rect()
+    r_1, r_2 = (randrange(WIDTH), randrange(HEIGHT))
+    bomb_rct.center = r_1, r_2
+    
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
@@ -17,9 +27,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+            
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        
+        # 練習問題1,表示
+        screen.blit(bomb, bomb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(10)
